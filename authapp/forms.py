4f54a0ef2c0f3f3d60bdc86965d5744ae2ea95ef
Yaroslav <yariksp@mail.ru>
2021-01-05 15:13:53 +0300

    - authenticate

---
+++ authapp/forms.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+
+from authapp.models import ShopUser
+
+
+class ShopUserAuthenticationForm(AuthenticationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = f'form-control {field_name}'
+
+
+class ShopUserRegisterForm(UserCreationForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'email', 'password1', 'password1')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = f'form-control {field_name}'
+
+
+class ShopUserProfileForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = f'form-control {field_name}'
--- authapp/views.py
+++ authapp/views.py
@@ -1,12 +1,48 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from authapp.forms import ShopUserAuthenticationForm, ShopUserRegisterForm, ShopUserProfileForm
+
 
 def login(request):
-    pass
+    if request.method == 'POST':
+        form = ShopUserAuthenticationForm(data=request.POST)
+        if form.is_valid():  # errors dict
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = ShopUserAuthenticationForm()
+    context = {
+        'page_title': 'аутентификация',
+        'form': form
+    }
+    return render(request, 'authapp/login.html', context)
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('main:index'))
 
 
-def register(request):
-    pass
+def user_register(request):
+    form = ShopUserRegisterForm()
+    context = {
+        'page_title': 'регистрация',
+        'form': form
+    }
+    return render(request, 'authapp/login.html', context)
+
+
+def user_profile(request):
+    form = ShopUserProfileForm()
+    context = {
+        'page_title': 'профиль',
+        'form': form
+    }
+    return render(request, 'authapp/login.html', context)
